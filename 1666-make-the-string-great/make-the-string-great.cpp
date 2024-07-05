f2adb8921f6436b90010b28f7f5087f86356637d
Maaz Iqbal <111086993+maaziqbal32@users.noreply.github.com>
2024-07-05 12:13:35 +0500

    Time: 0 ms (100.00%) | Memory: 7.8 MB (51.64%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string makeGood(string s) {
+       vector<char>st; 
+       st.push_back(s[0]);
+       for(int i=1;i<s.size();i++){
+        char TopVal=st.back();
+        if(!st.empty() && abs(TopVal-s[i])==32){
+            st.pop_back();
+        }else{
+            st.push_back(s[i]);
+        }
+        
+       }
+       string ans(st.begin(),st.end());
+       return ans;
+    }
+};

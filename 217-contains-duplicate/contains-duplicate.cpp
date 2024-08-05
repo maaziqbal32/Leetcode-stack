4570536ac2f317c217ac65d0d7ea3adc73a3133e
Maaz Iqbal <111086993+maaziqbal32@users.noreply.github.com>
2024-08-05 23:44:05 +0500

    Time: 106 ms (28.59%) | Memory: 75.5 MB (12.96%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+       unordered_set<int>s(nums.begin(),nums.end());
+       int x=nums.size();
+       int y=s.size();
+       if(x==y){
+        return false;
+       }
+       else
+       {
+ return true;
+       }
+    }
+  
+};

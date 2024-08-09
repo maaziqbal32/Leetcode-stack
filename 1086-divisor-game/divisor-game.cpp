76a4462d2461224e658d8839420413425c0793f0
Maaz Iqbal <111086993+maaziqbal32@users.noreply.github.com>
2024-08-09 16:14:01 +0500

    Time: 0 ms (100.00%) | Memory: 7.4 MB (34.82%) - LeetSync

---
+++ 1086-divisor-game/divisor-game.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool divisorGame(int n) {
+        for(int i=0;i<n;i++){
+            if(n%2==0 ) return true;
+        }
+         return false ;     
+                  
+    }
+};

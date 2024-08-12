c5f12c5679057790b10cb8357959701e5509359f
Maaz Iqbal <111086993+maaziqbal32@users.noreply.github.com>
2024-08-13 00:26:15 +0500

    Time: 2 ms (42.62%) | Memory: 7.4 MB (79.54%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        std::vector<int> aboveRow(n, 1);
-
-        for (int row = 1; row < m; row++) {
-            std::vector<int> currentRow(n, 1);
-            for (int col = 1; col < n; col++) {
-                currentRow[col] = currentRow[col - 1] + aboveRow[col];
+        // Initialize the dp array with 1s for the first row and column
+        int dp[m][n];
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                // Fill the first row and column with 1s
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 1;
+                } else {
+                    // Compute the number of unique paths for other cells
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
             }
-            aboveRow = currentRow;
         }
 
-        return aboveRow[n - 1];        
+        // Return the result from the bottom-right corner of the array
+        return dp[m-1][n-1];
     }
 };

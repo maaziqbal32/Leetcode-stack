94e44b6fed04df98506b594456e9a17a36b01f6a
Maaz Iqbal <111086993+maaziqbal32@users.noreply.github.com>
2024-09-03 12:13:58 +0500

    Time: 56 ms (42.85%) | Memory: 21.2 MB (87.24%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int left = 0;
+        int right = s.size() - 1;
+
+        while (left < right) {
+            if (s[left] != s[right]) {
+                // Try skipping the character at 'left' or 'right'
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+
+        return true;  // If we never needed to skip any characters, it's a palindrome
+    }
+
+private:
+    // Helper function to check if a substring s[left:right] is a palindrome
+    bool isPalindrome(const string& s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
